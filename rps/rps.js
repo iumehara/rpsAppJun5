354480f599c6ad95f98ce4d5296f9e0345918052
Robert Gravina, rgravina <rgravina@pivotal.io>
2019-06-03 17:14:37 +0900

    First two tests.
    Signed-off-by: Ichizo Umehara, iumehara <iumehara@pivotal.io>

--- rps/rps.js
+++ rps/rps.js
@@ -1,5 +1,11 @@
-const play = () => {
-    return 'Player 1 Wins';
-};
+class Requests {
+    play(p1, p2, observer) {
+        if (p2 === 'rock') {
+            observer.p2Wins();
+        } else {
+            observer.p1Wins();
+        }
+    }
+}
 
-module.exports = {play};
+module.exports = {Requests};
--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,7 +1,19 @@
-const {play} = require("../rps");
+const {Requests} = require("../rps");
 
 describe('play specs', () => {
     it('rock versus scissors', () => {
-        expect(play('rock', 'scissors')).toBe('Player 1 Wins');
-    })
+        const observer = jasmine.createSpyObj('observer', ['p1Wins']);
+
+        expect(new Requests().play('rock', 'scissors', observer));
+
+        expect(observer.p1Wins).toHaveBeenCalled();
+    });
+
+    it('scissors versus rock', () => {
+        const observer = jasmine.createSpyObj('observer', ['p2Wins']);
+
+        expect(new Requests().play('scissors', 'rock', observer));
+
+        expect(observer.p2Wins).toHaveBeenCalled();
+    });
 });
