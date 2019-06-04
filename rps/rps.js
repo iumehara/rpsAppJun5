5d585306907383cb0e81d3795bd647d7e9283aa4
Robert Gravina <rgravina@pivotal.io>
2019-06-04 12:26:42 +0900

    Refactoring

--- rps/rps.js
+++ rps/rps.js
@@ -1,32 +1,47 @@
-const ROCK = 'rock';
-const PAPER = 'paper';
-const SCISSORS = 'scissors';
-const VALID_MOVES = ['rock', 'paper', 'scissors'];
+const THROW = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+};
 
-class Requests {
-    play(p1, p2, observer) {
+const VALID_MOVES = [THROW.ROCK, THROW.PAPER, THROW.SCISSORS];
+
+function Requests() {
+    this.play = (p1, p2, observer) => {
         new PlayRoundRequest(p1, p2, observer).process();
     }
 }
 
 function PlayRoundRequest(p1, p2, observer) {
     this.process = () => {
-        if (VALID_MOVES.includes(p1) === false || VALID_MOVES.includes(p2) === false) {
-            return;
+        if (invalid(p1) || invalid(p2)) {
+            return
         }
 
-        if (p1 === p2) {
+        if (draw()) {
             observer.tie();
-            return;
+            return
         }
 
-        if (p1 === ROCK && p2 === SCISSORS
-            || p1 === SCISSORS && p2 === PAPER
-            || p1 === PAPER && p2 === ROCK) {
-            observer.p1Wins();
+        if (p1Wins()) {
+            observer.p1Wins()
         } else {
-            observer.p2Wins();
+            observer.p2Wins()
         }
+    };
+
+    const invalid = (choice) => {
+        return VALID_MOVES.includes(choice) === false
+    };
+
+    const draw = () => {
+        return p1 === p2
+    };
+
+    const p1Wins = () => {
+        return p1 === THROW.ROCK && p2 === THROW.SCISSORS
+            || p1 === THROW.SCISSORS && p2 === THROW.PAPER
+            || p1 === THROW.PAPER && p2 === THROW.ROCK;
     }
 }
 
