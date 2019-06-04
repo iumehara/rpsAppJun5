4b8a0b518618501d2d8cbf5c2e7ac906706c3a8c
Robert Gravina <rgravina@pivotal.io>
2019-06-04 14:35:50 +0900

    Add test for invalid input with simple implementation.

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,21 @@
-describe('RPS App', function () {
-  it('wiring test', function () {
-    expect(true).toBe(true)
-  })
-})
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PlayForm from '../src/PlayForm';
+
+describe('play form', () => {
+    describe('when the play use case tells the UI the input is invalid', () => {
+        it('tells the user their input is invalid', () => {
+            const domFixture = document.createElement('div');
+            document.body.appendChild(domFixture);
+
+            const alwaysInvalidRequest = {
+                play: (p1, p2, observer) => observer.invalid()
+            };
+
+            ReactDOM.render(<PlayForm requests={alwaysInvalidRequest}/>, domFixture);
+
+            document.querySelector('button').click();
+            expect(domFixture.innerText).toContain('INVALID!')
+        });
+    });
+});
---
+++ web/src/PlayForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlayForm = () => {
+    return <button>INVALID!</button>;
+};
+
+export default PlayForm;
