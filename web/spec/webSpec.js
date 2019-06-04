fa13aea6f9bb5497e9ee8dbe656ab1e70ea74ec1
pivotal <softwhitedog@gmail.com>
2019-06-04 12:22:29 +0900

    First invalid test

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,21 @@
-describe('RPS App', function () {
-  it('wiring test', function () {
-    expect(true).toBe(true)
-  })
+import ReactDOM from "react-dom";
+import React from 'react'
+import PlayForm from "../src/PlayForm";
+
+
+describe('Play Form', function () {
+  it('tells the user the input is invalid', function () {
+    const domFixutre = document.createElement('div')
+    document.querySelector('body').appendChild(domFixutre)
+    ReactDOM.render(
+        <PlayForm />,
+        domFixutre
+    )
+
+
+    document.querySelector('button').click()
+
+
+    expect(domFixutre.innerText).toContain('INVALID')
+  });
 })
---
+++ web/src/PlayForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class PlayForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>send</button>
+                <div>INVALID</div>
+            </div>
+        )
+    }
+}
+
+export default PlayForm
