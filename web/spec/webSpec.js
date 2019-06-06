c9889d46006c72337172c87e21603aabbba0a246
Robert Gravina <rgravina@pivotal.io>
2019-06-06 15:56:13 +0900

    display form title and labels

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,39 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import PlayForm from '../src/PlayForm'
+
 describe('RPS App', function () {
-  it('wiring test', function () {
-    expect(true).toBe(true)
+  let domFixture
+
+  function displayApp() {
+    ReactDOM.render(<PlayForm/>, domFixture)
+  }
+
+  beforeEach(() => {
+    domFixture = document.createElement('div')
+    document.querySelector('body').appendChild(domFixture)
+  })
+
+  afterEach(() => {
+    domFixture.remove()
+  })
+
+  describe('displays form', () => {
+    it('with application name', function () {
+      displayApp()
+      expect(domFixture.innerText).toContain('Janken Game')
+    })
+    it('with p1 input', function () {
+      displayApp()
+      expect(domFixture.innerText).toContain('P1')
+    })
+    it('with p2 input', function () {
+      displayApp()
+      expect(domFixture.innerText).toContain('P2')
+    })
+    it('with play button', function () {
+      displayApp()
+      expect(domFixture.querySelector('button').innerText).toContain('Play')
+    })
   })
 })
---
+++ web/src/PlayForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class PlayForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>Janken Game</div>
+                <div>
+                    <div>P1</div>
+                </div>
+                <div>
+                    <div>P2</div>
+                </div>
+                <button>Play</button>
+            </div>
+        )
+    }
+}
